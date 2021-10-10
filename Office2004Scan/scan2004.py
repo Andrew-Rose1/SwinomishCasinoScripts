2147af95290ec868b9e08e1bbec8cd86898d3998
Andrew-Rose1 <55816533+Andrew-Rose1@users.noreply.github.com>
2021-10-09 17:30:55 -0700

    Delete scan2004.py

--- Office2004Scan/scan2004.py
+++
@@ -1,9 +0,0 @@
-import os
-import csv
-
-path = 'C:\\Users\\arose\\Programs\\MicrosoftVersionCSV'
-
-file = open(path, "r")
-reader = csv.reader(file, delimiter=',')
-for row in reader:
-    print(col[4])
