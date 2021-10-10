34b3707a080c0e136a55dd8e0b8ce8f45f8068a4
Andrew-Rose1 <55816533+Andrew-Rose1@users.noreply.github.com>
2021-10-09 17:27:37 -0700

    Update DisabledActiveSync.ps1

--- DisableActiveSync/DisabledActiveSync.ps1
+++ DisableActiveSync/DisabledActiveSync.ps1
@@ -1,9 +1,9 @@
-﻿$User = "NLC\Accounts"
-$Pass = ConvertTo-SecureString -String "(RXIYu:~C!m" -AsPlainText -Force
+﻿$User = "**********"
+$Pass = ConvertTo-SecureString -String "**********" -AsPlainText -Force
 $Credential = New-Object -TypeName "System.Management.Automation.PSCredential" -ArgumentList $User, $Pass
 $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://Athena.nlc.com/PowerShell/ -Authentication Kerberos -Credential $Credential
 Import-PSSession $Session
 
-#$acc = Read-Host -Promt "What account would you like to disable?"
+$acc = Read-Host -Promt "What account would you like to disable?"
 
-Set-CASMailbox -Identity "ttestd" -ActiveSyncEnabled $false
+Set-CASMailbox -Identity $acc -ActiveSyncEnabled $false
